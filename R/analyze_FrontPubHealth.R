a5e22965f33172e7a677f563915378ca963b8d7f
Matthew Turner <maturner01@gmail.com>
2026-03-11 06:19:48 -0700

    updates for start of 2026 Instituto Year

--- R/analyze_FrontPubHealth.R
+++ R/analyze_FrontPubHealth.R
@@ -8,7 +8,7 @@
 # 
 # 
 # Author: Matthew A. Turner <maturner@stanford.edu>
-# Date: 30 May 2024
+# Date: 11 March 2026
 #
 
 
@@ -17,29 +17,35 @@ library(ggplot2)
 library(lubridate)
 library(readxl)
 
+# Custom theme for plots
 mytheme <- theme(axis.line = element_line(), legend.key=element_rect(fill = NA),
                  text = element_text(size=16),# family = 'PT Sans'),
                  legend.key.width = unit(1, 'cm'),
                  legend.key.size = unit(2.5, 'lines'),
+                 axis.text.x = element_text(angle = 35, hjust = 1),
                  panel.background = element_rect(fill = "white"))
 
-# Load variant table 
+# Create table 
 variant_tbl <- 
-  read_excel("data/supp table- GISAID.xlsx", skip = 2) %>% 
-  rename(Variant = `SARS-CoV-2 variants (Pangolin classification)`, 
+  # First load variant table from original Excel, piped to next line
+  read_excel("data/LaísIzaSimoneEtAl-FrPubHealth2023.xlsx", skip = 2) %>% 
+  # Table piped from prev line passed to rename columns for convenience
+  dplyr::rename(Variant = `SARS-CoV-2 variants (Pangolin classification)`, 
          Village = `Indigenous village`,
          Ethnicity = `Indigenous ethnicity`,
          Date = `Collecting date (yyyy-MM-dd)`)
 
+# Ensure that the Date column is in most compatible format for ggplot
 variant_tbl$Date <- lubridate::date(variant_tbl$Date)
 variant_tbl$case <- 1
 
+# Create plot across listed dimensions assumes variant_tbl exists
 plot_variant_cases_by_village <- 
   function(variants = c("B.1.1", "Gamma (P.1)", "Zeta (P.2)"),
-           savepath = "plots/variant_by_village_series.pdf") {
+           savepath = "variant_by_village_series.png") {
 
   # Create plot of cumulative cases for different variants by village.
-  p <- 
+  data_tbl <- 
     # Make time series data grouped by variant and village by first...
     variant_tbl %>%
       # ...selecting desired variants and villages...
@@ -49,27 +55,33 @@ plot_variant_cases_by_village <-
       # ...sort by ascending date,...
       arrange(Date) %>%
       # ...and aggregate over dummy `case` column, passing result to plotting.
-      mutate(`Cumulative cases` = cumsum(case)) %>%
+      mutate(`Cumulative cases` = cumsum(case))
     
+    p <-
       # Plot the cumulative cases using village for linetype, variant for color.
-      ggplot(aes(x = Date, y = `Cumulative cases`, shape = Village, linetype = Village, color = Variant)) +
-        geom_line(size=1.05) + geom_point(size = 2.2) +
+      ggplot(
+        data_tbl,
+        aes(x = Date, y = `Cumulative cases`, shape = Village, 
+            linetype = Village, color = Variant)) +
+        geom_line(linewidth = 1.05) + geom_point(size = 2.2) +
         # Customize Date x-axis ticks.
-        scale_x_date(date_breaks = "months" , date_labels = "%b-%y") +
+        scale_x_date(date_breaks = "months" , date_labels = "%b %Y") +
         mytheme
   
-  ggsave(savepath, width=10, height=4.5)
+  # Save the figure to file
+  ggsave(savepath, width = 10, height = 5.0)
   
+  # The function returns the ggplot in case user wants to do more with it
   return (p)
 }
 
 # 
 plot_variant_cases_by_ethnicity <- 
   function(variants = c("B.1.1", "Gamma (P.1)", "Zeta (P.2)"),
-           savepath = "plots/variant_by_ethnicity_series.pdf") {
+           savepath = "variant_by_ethnicity_series.png") {
     
   # Create plot of cumulative cases for different variants by village.
-  p <- 
+  data_tbl <- 
     # Make time series data grouped by variant and village by first...
     variant_tbl %>%
     # ...selecting desired variants and villages...
@@ -79,22 +91,29 @@ plot_variant_cases_by_ethnicity <-
     # ...sort by ascending date,...
     arrange(Date) %>%
     # ...and aggregate over dummy `case` column, passing result to plotting.
-    mutate(`Cumulative cases` = cumsum(case)) %>%
-    
+    mutate(`Cumulative cases` = cumsum(case)) 
+
+  p <-
     # Plot the cumulative cases using village for linetype, variant for color.
-    ggplot(aes(x = Date, y = `Cumulative cases`, shape = Ethnicity, linetype = Ethnicity, color = Variant)) +
-    geom_line(size=1.05) + geom_point(size = 2.2) +
+    ggplot(data_tbl,
+      aes(x = Date, y = `Cumulative cases`, shape = Ethnicity, 
+          linetype = Ethnicity, color = Variant)) +
+    geom_line(linewidth = 1.05) + geom_point(size = 2.2) +
     # Customize Date x-axis ticks.
-    scale_x_date(date_breaks = "months" , date_labels = "%b-%y") +
-    mytheme
+    scale_x_date(date_breaks = "months" , date_labels = "%b %Y") +
+    mytheme 
   
-  ggsave(savepath, width=10.5, height=4.5)
+  ggsave(savepath, width = 10.5, height = 5.0)
   
   return (p)
 }
 
 
 # Make the plot for these two variants. There was only one Gamma case in Bororó.
-p <- plot_variant_cases_by_village(variants = c("B.1.1", "Zeta (P.2)"))
-p <- plot_variant_cases_by_ethnicity(variants = c("B.1.1", "Zeta (P.2)"))
-print(p)
+p1 <- plot_variant_cases_by_village(variants = c("B.1.1", "Zeta (P.2)"))
+p2 <- plot_variant_cases_by_ethnicity(variants = c("B.1.1", "Zeta (P.2)"))
+
+# What we want to be able to do ("Week's Activities" in the Week 1 Google Doc)
+# new_data_path <- "data/new_data.xlsx"
+# p1 <- plot_variant_cases_by_village("data/copy-test.xlsx", 
+#                                     variants = c("B.1.1", "Zeta (P.2)"))
--- README.md
+++ README.md
@@ -1,28 +1,67 @@
-# disease-diffusion
+# A first R script to analyze some publicly-available data from Iza, Laís, and Simone's paper.
 
-This code base is using the [Julia Language](https://julialang.org/) and
-[DrWatson](https://juliadynamics.github.io/DrWatson.jl/stable/)
-to make a reproducible scientific project named
-> disease-diffusion
+Instituto Dourados researchers Iza Rezende, Laís Albuquerque, and Simone
+Simionatto tracked how different COVID-19 variants spread in an Indigenous
+population composed of diverse Indigenous ethnicities, and residing in different
+towns, one closer to the neighboring city (Albuquerque, Rezende, ...,
+Simionatto, 2023).
 
-To (locally) reproduce this project, do the following:
+Given this new fine-grained perspective on pandemics in Indigenous populations,
+the authors could make new hypotheses about what they observed. Right or wrong,
+evaluating these hypotheses can support Indigenous public health by contributing
+to a growing knowledge base of epidemiological dynamics in Indigenous
+populations.
 
-0. Download this code base. Notice that raw data are typically not included in the
-   git-history and may need to be downloaded independently.
-1. Open a Julia console and do:
-   ```
-   julia> using Pkg
-   julia> Pkg.add("DrWatson") # install globally, for using `quickactivate`
-   julia> Pkg.activate("path/to/this/project")
-   julia> Pkg.instantiate()
-   ```
+Laís, Iza, and Simone made their data publicly available, and it is included
+with this repository in the  
 
-This will install all necessary packages for you to be able to run the scripts and
-everything should work out of the box, including correctly finding local paths.
+## Usage:
 
-You may notice that most scripts start with the commands:
-```julia
-using DrWatson
-@quickactivate "disease-diffusion"
+### In an R shell
+
+```R
+source("R/analyze_FrontPubHealth.R")
 ```
-which auto-activate the project and enable local path handling from DrWatson.
+
+### In a terminal
+
+```
+Rscript -e 'source("R/analyze_FrontPubHealth.R")'
+```
+
+### The result
+
+The two resulting plots visualize the cumulative number of infections recorded
+over time, with separate series for each combination of data dimensions of
+variant and ethnicity, and each combination of variant and "village", or
+_aldeia_ to use the Brazilian Portuguese term for a village in a _Reserva
+Indígena_ in Brazil. One thing we notice, for example, is that in all
+dimensional combinations, a large number of cases first spread through
+Jaguapiru, and then later through Bororó. This supports the hypothesis that more
+frequent travel to the city since Jaguapiru is the aldeia that borders the city
+of Dourados.
+
+Two plots are created and written to png by default:
+`variant_by_ethnicity_series.pdf` and `variant_by_village_series.pdf`. Both plot
+cumulative infections in the Reserva from July 2020 to March 2021.
+
+#### Variant × ethnicity epidemic curves
+
+![](variant_by_ethnicity_series.png)
+
+#### Variant × village epidemic curves
+
+![](variant_by_village_series.png)
+
+### The data
+
+are publicly available, published to support the following paper:
+
+> **de Oliveira, L. A.**, **de Rezende, I. M.**, Navarini, V. J., Marchioro, S.
+> B., Torres, A. J. L., Croda, J., Croda, M. G., Gonçalves, C. C. M., Xavier,
+> J., de Castro, E., Lima, M., Iani, F., Adelino, T., Aburjaile, F., Ferraz
+> Demarchi, L. H., Taira, D. L., Zardin, M. C. S. U., Fonseca, V., Giovanetti,
+> M., … **Simionatto, S.** (2023). Genomic characterization of SARS-CoV-2 from
+> an indigenous reserve in Mato Grosso do Sul, Brazil. 
+> _Frontiers in Public Health_, 11(October), 1–11. 
+> \[[link](https://doi.org/10.3389/fpubh.2023.1195779)\]
